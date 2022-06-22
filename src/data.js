e4f2e153db8def057efecd48496343a0c494f19a
Daniel Lucena <daniel.lucena@olxbr.com>
2022-06-22 18:30:15 -0300

    add data.js

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const anuncios = [
+    {
+        id: 1,
+        name: 'Iphone 11 - 64gb',
+        category: 'Eletronics',
+        price: '3.000,00',
+    },
+    {
+        id: 2,
+        name: 'Notebook Deel',
+        category: 'Eletronics',
+        price: '4.000,00',
+    },
+    {
+        id: 3,
+        name: 'Casa com piscina',
+        category: 'House',
+        price: '600.000,00',
+    },
+    {
+        id: 4,
+        name: 'Apartamento com garagem',
+        category: 'House',
+        price: '550.000,00',
+    },
+];
